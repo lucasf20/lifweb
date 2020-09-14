699824143d9f466b71e0db8eb7f8066b4740026e
viguime <jvictormguimaraes@gmail.com>
2020-09-14 01:56:31 -0300

    restructured the views , text inputs, etc

--- src/pages/CreateAcc/index.js
+++ src/pages/CreateAcc/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Feather} from '@expo/vector-icons';
-import {View,Text, Image, TouchableOpacity, Button} from 'react-native';
+import {ScrollView, View,Text, Image, TouchableOpacity, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import { TextInput } from 'react-native';
+import MyTextInput from '../../MyTextInput';
 
 import lifweb from '../../assets/logolifweb.png';
 
@@ -17,40 +17,35 @@ export default function CreateAcc(){
     }
     
     return(
-        <View style = {styles.container}>
+        
+        <ScrollView style = {styles.container}>
+
             <View style={styles.header}>
 
                 <TouchableOpacity onPress={navigateBack}>
                     <Feather name = "arrow-left" size = {28} />
                 </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                        INICIO 
+                    </Text>
+                </TouchableOpacity>
+
             </View>  
 
-            <View style = {styles.container}>
-
-                <View styles = {styles.Login}>
-                    <Image
-                        source = {lifweb}
-                    />
-                </View>
-
-                <Text style ={[styles.incidentProperty, {marginTop:0}]}>Email</Text>
-                <TextInput 
-                    style={styles.TextBox}
-                    //onChangeText={text => (' ')}
-                    value={''}
+            <View style = {styles.logo}>
+                <Image
+                    source = {lifweb}
                 />
-                
-                
-               
             </View>
 
-            <View style = {styles.container}>
-                    <Button style ={[{marginTop:300}]}
-                        title= "Criar uma Conta" 
-                    />
-            </View>
-            
-        </View>
+            <MyTextInput/>
+                
+            <Button 
+                style = {styles.Login}
+                title= "Criar uma Conta" 
+            />
+        </ScrollView>
     );
 }
 
