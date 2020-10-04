344cfc52d10782223e051f08ccbffb95d08ec03d
Lucas Ferreira <ferreira.lucas639@gmail.com>
2020-10-04 09:43:49 -0300

    setting headers icons

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
 import Icon from '../../images/avatar_stories1.png'
 import styles from './styles'
-import { SimpleLineIcons } from '@expo/vector-icons'; 
+import home from '../../assets/home_icon.png'
+import { SimpleLineIcons } from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native'; 
 
 
 function Header(){
+    const navigation = useNavigation()
+    
    return(
        <View style={styles.container}>
-           <SimpleLineIcons name="camera" size={24} color="black" />
-           <Text style={styles.title}>Lifweb</Text>
-           <SimpleLineIcons name="paper-plane" size={24} color="black" />
+           <SimpleLineIcons name="menu" size={24} color="black" />
+           <TouchableOpacity onPress={() => {navigation.navigate("Feed")}}>
+               <Image source={home} style={{height:50, width:170}}>
+               </Image>
+           </TouchableOpacity>
+           <SimpleLineIcons name="bubbles" size={24} color="black" />
        </View>
    )
 }
