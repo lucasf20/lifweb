4b4b2e600aa6ed2e653d114ad049bc348ba3333d
Lucas Ferreira <ferreira.lucas639@gmail.com>
2020-10-08 16:22:43 -0300

    adding profile icon on menu

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -9,22 +9,42 @@ import styles from './styles';
 import colorStyles from "../../colors";
 import firebase from '../../../firebaseConfig';
 
+import profileIcon from '../../assets/logolifweb.png'
+
 export default function Menu() {
 
     const dorange = colorStyles.dorange
     const navigation = useNavigation();
 
-    function logout(){
+    function logout() {
         firebase.auth().signOut()
-        .then(navigation.navigate('Login'))
+            .then(navigation.navigate('Login'))
     }
 
+    function resizeProfile(scale_factor){
+        const {width, height} = Image.resolveAssetSource(profileIcon)
+        return {width: width*scale_factor, height:height*scale_factor}
+    }
+
+    function getName(){
+        var name;
+        const user =firebase.auth().currentUser
+        firebase.database().ref('user/' + user.uid + '/apelido/').on('value', snapshot=>{
+            name = snapshot.val()
+        })
+        return name
+    }
+
+    function getEmail(){
+        const user = firebase.auth().currentUser
+        return user.email
+    }
     return (
-        <View style={{...styles.container,paddingTop:25, paddingHorizontal:18}}>
-            <TouchableOpacity onPress={() => {navigation.navigate("Feed")}}>
-               <SimpleLineIcons name="menu" size={24} color="white" style={{marginTop:23, paddingHorizontal:(-18)}}/>
-           </TouchableOpacity>
-            <ScrollView style={{...styles.container, paddingHorizontal:18}}>
+        <View style={{ ...styles.container, paddingTop: 25, paddingHorizontal: 18 }}>
+            <TouchableOpacity onPress={() => { navigation.navigate("Feed") }}>
+                <SimpleLineIcons name="menu" size={24} color="white" style={{ marginTop: 23, paddingHorizontal: (-18) }} />
+            </TouchableOpacity>
+            <ScrollView style={{ ...styles.container, paddingHorizontal: 18 }}>
                 <View style={{ paddingTop: 75 }}>
                     <TouchableOpacity style={{ ...styles.buttons, borderTopColor: 'transparent' }}>
                         <SimpleLineIcons name="bubbles" size={24} color="white" style={{ paddingVertical: 20 }} />
@@ -68,12 +88,23 @@ export default function Menu() {
                             Configurações
                 </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.buttons} onPress={()=>{logout()}}>
-                        <SimpleLineIcons name="logout" size={24} color="white" style={{ paddingVertical: 20 }}/>
+                    <TouchableOpacity style={styles.buttons} onPress={() => { logout() }}>
+                        <SimpleLineIcons name="logout" size={24} color="white" style={{ paddingVertical: 20 }} />
                         <Text style={styles.BigText}>
                             Fazer Logoff
                 </Text>
                     </TouchableOpacity>
+                </View>
+                <View style={{marginTop: 40, marginBottom:20}}>
+                    <Image source={profileIcon} style={{height:resizeProfile(0.5).height,width:resizeProfile(0.5).width}}/>
+                    <Text style={{fontSize:25, color:'white'}}>
+                        {
+                         getName()   
+                        }
+                    </Text>
+                    <Text style={{fontSize:13, color:'white'}}>
+                        {getEmail()}
+                    </Text>
                 </View>
             </ScrollView>
         </View>
