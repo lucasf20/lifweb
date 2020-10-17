da6030e3d9ec351d478b9e0c5d6ac34d39a3cb7b
Lucas Ferreira <ferreira.lucas639@gmail.com>
2020-10-17 20:20:02 -0300

    adding grid

---
+++ src/pages/Profile/Timeline/index.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { ScrollView, View, Image, Text, Dimensions, TouchableOpacity, FlatList } from 'react-native'
+import { SimpleLineIcons } from '@expo/vector-icons'
+
+import image from '../../../images/avatar_stories1.jpg'
+import image2 from '../../../images/avatar_stories2.jpg'
+import image3 from '../../../images/avatar_stories3.jpg'
+import image4 from '../../../images/fotocapa.jpg'
+import image5 from '../../../images/perfil3.jpg'
+import image6 from '../../../images/post_image.jpg'
+import image7 from '../../../images/post_image.png'
+import image8 from '../../../images/PostImage2.png'
+import image9 from '../../../images/PostImage3.png'
+
+
+
+
+function Grid({ imagesList }) {
+    var lines = []
+
+    function showItem(item, index) {
+        if (item.length == 2) {
+            if (index % 2 == 0) {
+                return (
+                    <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                        <TouchableOpacity>
+                            <Image source={item[0]} style={{ borderRadius: 5, height: 110, width: ((Dimensions.get('window').width - 60) * (2 / 3)) }} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image source={item[1]} style={{ marginLeft: 20, borderRadius: 5, height: 110, width: ((Dimensions.get('window').width - 60) * (1 / 3)) }} />
+                        </TouchableOpacity>
+                    </View>
+                )
+            } else {
+                return (
+                    <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                        <TouchableOpacity>
+                            <Image source={item[0]} style={{ borderRadius: 5, height: 110, width: ((Dimensions.get('window').width - 60) * (1 / 3)) }} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Image source={item[1]} style={{ marginLeft: 20, borderRadius: 5, height: 110, width: ((Dimensions.get('window').width - 60) * (2 / 3)) }} />
+                        </TouchableOpacity>
+                    </View>
+                )
+            }
+        } else {
+            if (index % 2 == 0) {
+                return (
+                    <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                        <TouchableOpacity>
+                            <Image source={item[0]} style={{ borderRadius: 5, height: 110, width: ((Dimensions.get('window').width - 60) * (2 / 3)) }} />
+                        </TouchableOpacity>
+                    </View>
+                )
+            } else {
+                return (
+                    <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+                        <TouchableOpacity>
+                            <Image source={item[0]} style={{ borderRadius: 5, height: 110, width: ((Dimensions.get('window').width - 60) * (1 / 3)) }} />
+                        </TouchableOpacity>
+                    </View>
+                )
+            }
+        }
+    }
+
+    for (let i = 0; i < imagesList.length; i++) {
+        if (i + 1 < imagesList.length) {
+            lines.push([imagesList[i], imagesList[++i]])
+        } else {
+            lines.push([imagesList[i]])
+        }
+    }
+
+    return (
+        <ScrollView style={{ marginBottom:(240*lines.length) }} >
+            {lines.map((item, index, arr) => (showItem(item, index)))}
+        </ScrollView>
+
+    )
+}
+
+export default function Timeline() {
+
+    var imglist = [image2, image3, image4, image5, image7, image8, image9]
+
+    return (
+        <ScrollView>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 20 }}>
+                <Text style={{ fontSize: 20 }}>
+                    Timeline
+                </Text>
+                <TouchableOpacity style={{ marginTop: 5 }}>
+                    <SimpleLineIcons name="options" size={24} color="gray" />
+                </TouchableOpacity>
+            </View>
+            <View style={{marginHorizontal:20}}>
+                <Grid imagesList={imglist} />
+            </View>
+        </ScrollView>
+    )
+}
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -9,6 +9,7 @@ import Svg, {
 import { SimpleLineIcons, EvilIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from './styles'
 import Cambutton from '../../Components/Cambutton'
+import Timeline from './Timeline'
 import { useNavigation } from '@react-navigation/native'
 import colorStyles from "../../colors";
 
@@ -26,74 +27,75 @@ export default function Profile() {
     return (
         <View>
             <View>
-                <Image source={capa}
-                    style={{ width: calculateDimensions(capa).width, height: calculateDimensions(capa).height }}
-                />
-                <TouchableOpacity style={{ position: 'absolute', marginLeft: 15, marginTop: 30 }} onPress={() => { navigation.navigate("Menu") }}>
-                    <SimpleLineIcons name="menu" size={24} color="white" />
-                </TouchableOpacity>
-                <Text style={{ fontWeight: 'bold', color: 'white', position: 'absolute', marginLeft: (Dimensions.get('window').width / 2 - 15), marginTop: 30, fontSize: 20 }}>
-                    Perfil
+                    <Image source={capa}
+                        style={{ width: calculateDimensions(capa).width, height: calculateDimensions(capa).height }}
+                    />
+                    <TouchableOpacity style={{ position: 'absolute', marginLeft: 15, marginTop: 30 }} onPress={() => { navigation.navigate("Menu") }}>
+                        <SimpleLineIcons name="menu" size={24} color="white" />
+                    </TouchableOpacity>
+                    <Text style={{ fontWeight: 'bold', color: 'white', position: 'absolute', marginLeft: (Dimensions.get('window').width / 2 - 15), marginTop: 30, fontSize: 20 }}>
+                        Perfil
                     </Text>
-                <TouchableOpacity style={{ position: 'absolute', marginLeft: (Dimensions.get('window').width - 80), marginTop: 30 }}>
-                    <EvilIcons name="search" size={30} color="white" style={{ paddingRight: 15 }} />
-                </TouchableOpacity>
-                <TouchableOpacity style={{ position: 'absolute', marginLeft: (Dimensions.get('window').width - 40), marginTop: 30 }} onPress={() => { navigation.navigate('Direct') }}>
-                    <MaterialCommunityIcons name="message-outline" size={24} color="white" />
-                </TouchableOpacity>
-                <View style={{ position: "absolute", marginLeft: 25, marginTop: (calculateDimensions(capa).height - 50), flexDirection: 'row' }}>
-                    <TouchableOpacity >
-                        <Svg width="116" height="116" viewBox="0 0 50 50">
-                            <Defs>
-                                <ClipPath id="image" clipRule="evenodd">
-                                    <Polygon points="0 10, 22.5 0, 45 10, 45 40, 22.5 50, 0 40" />
-                                </ClipPath>
-                            </Defs>
-                            <SvgImage
-                                x="0"
-                                y="0"
-                                width="50"
-                                height="50"
-                                href={icon}
-                                clipPath="#image"
-                            />
-                        </Svg>
+                    <TouchableOpacity style={{ position: 'absolute', marginLeft: (Dimensions.get('window').width - 80), marginTop: 30 }}>
+                        <EvilIcons name="search" size={30} color="white" style={{ paddingRight: 15 }} />
                     </TouchableOpacity>
-                    <View>
-                        <Text style={{ fontSize: 25, marginTop: 50 }}>
-                            Nome da Pessoa
-                        </Text>
-                        <Text style={{ fontSize: 15, color: 'gray' }}>
-                            Profissão
-                        </Text>
-                        <Text style={{ fontSize: 15, fontWeight: 'bold' }}>
-                            Modelo da Moto
-                        </Text>
-                    </View>
-                </View>
-                <View style={{ marginTop: 90, borderWidth: 0.5, marginHorizontal: 20, height: 70, borderBottomColor: 'silver', borderTopColor: 'silver', borderLeftColor: 'transparent', borderRightColor: 'transparent', flexDirection:'row', justifyContent:'space-between' }}>
-                    <View>
-                        <Text style={{fontSize:25, marginTop:5}}>
-                            300K
-                        </Text>
-                        <Text style={{color:'gray', marginTop:5}}>
-                            Seguidores
-                        </Text>
-                    </View>
-                    <View>
-                        <Text style={{fontSize:25, marginTop:5}}>
-                            300
-                        </Text>
-                        <Text style={{color:'gray', marginTop:5}}>
-                            Seguindo
-                        </Text>
-                    </View>
-                    <TouchableOpacity style={{backgroundColor:colorStyles.dorange, marginVertical:5, width:150, borderRadius:5}}>
-                        <Text style={{color:'white', marginTop:15, marginLeft:45, fontSize:20}}>
-                            Seguir
-                        </Text>
+                    <TouchableOpacity style={{ position: 'absolute', marginLeft: (Dimensions.get('window').width - 40), marginTop: 30 }} onPress={() => { navigation.navigate('Direct') }}>
+                        <MaterialCommunityIcons name="message-outline" size={24} color="white" />
                     </TouchableOpacity>
-                </View>
+                    <View style={{ position: "absolute", marginLeft: 25, marginTop: (calculateDimensions(capa).height - 50), flexDirection: 'row' }}>
+                        <TouchableOpacity >
+                            <Svg width="116" height="116" viewBox="0 0 50 50">
+                                <Defs>
+                                    <ClipPath id="image" clipRule="evenodd">
+                                        <Polygon points="0 10, 22.5 0, 45 10, 45 40, 22.5 50, 0 40" />
+                                    </ClipPath>
+                                </Defs>
+                                <SvgImage
+                                    x="0"
+                                    y="0"
+                                    width="50"
+                                    height="50"
+                                    href={icon}
+                                    clipPath="#image"
+                                />
+                            </Svg>
+                        </TouchableOpacity>
+                        <View>
+                            <Text style={{ fontSize: 25, marginTop: 50 }}>
+                                Nome da Pessoa
+                        </Text>
+                            <Text style={{ fontSize: 15, color: 'gray' }}>
+                                Profissão
+                        </Text>
+                            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>
+                                Modelo da Moto
+                        </Text>
+                        </View>
+                    </View>
+                    <View style={{ marginTop: 90, borderWidth: 0.5, marginHorizontal: 20, height: 70, borderBottomColor: 'silver', borderTopColor: 'silver', borderLeftColor: 'transparent', borderRightColor: 'transparent', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View>
+                            <Text style={{ fontSize: 25, marginTop: 5 }}>
+                                300K
+                        </Text>
+                            <Text style={{ color: 'gray', marginTop: 5 }}>
+                                Seguidores
+                        </Text>
+                        </View>
+                        <View>
+                            <Text style={{ fontSize: 25, marginTop: 5 }}>
+                                300
+                        </Text>
+                            <Text style={{ color: 'gray', marginTop: 5 }}>
+                                Seguindo
+                        </Text>
+                        </View>
+                        <TouchableOpacity style={{ backgroundColor: colorStyles.dorange, marginVertical: 5, width: 150, borderRadius: 5 }}>
+                            <Text style={{ color: 'white', marginTop: 15, marginLeft: 45, fontSize: 20 }}>
+                                Seguir
+                        </Text>
+                        </TouchableOpacity>
+                    </View>
+                <Timeline />
             </View>
             <Cambutton />
         </View>
