eaf0718511fa747247f998d2a2ecf869e2376ab5
Lucas Ferreira <ferreira.lucas639@gmail.com>
2020-11-13 15:03:39 -0300

    fixing chat order

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -89,10 +89,10 @@ function Chat({ route }) {
           });
 
           aux.sort(function (a, b) {
-            if (a.ordem < b.ordem) {
+            if (a.createdAt < b.createdAt) {
               return 1;
             }
-            if (a.ordem > b.ordem) {
+            if (a.createdAt > b.createdAt) {
               return -1;
             }
             // a must be equal to b
