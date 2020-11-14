e3953b492aa4bb6915388494e481ec2dcb4a768e
Lucas Ferreira <ferreira.lucas639@gmail.com>
2020-11-14 09:41:59 -0300

    comments page ok

--- src/pages/Comments/index.js
+++ src/pages/Comments/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react"
-import { View, Dimensions, ScrollView, Image, Text } from 'react-native'
+import { View, Dimensions, ScrollView, Image, Text, Alert } from 'react-native'
 import Header from '../../Components/Header'
 import MyTextInput from '../../MyTextInput'
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
 import colorStyles from '../../colors'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import firebase from '../../../firebaseConfig'
@@ -13,10 +13,13 @@ import Svg, {
     ClipPath,
     Polygon,
 } from 'react-native-svg';
+import { useNavigation, StackActions } from '@react-navigation/native';
 
 function RenderComment({ comment }) {
     const [image, setimage] = useState(null)
     const [name, setname] = useState(null)
+    const canDelete = comment['user'] == firebase.auth().currentUser.uid
+    const nav = useNavigation()
 
     function getTime() {
         var now = Date.now()
@@ -46,51 +49,73 @@ function RenderComment({ comment }) {
         })
         return n
     }
+
+    async function deleteComment(){
+        await firebase.firestore().collection('posts').doc(comment['postname']).update({comments:firebase.firestore.FieldValue.arrayRemove(comment)})
+    }
     getImg(comment['user']).then(im => setimage(im))
     if (!name) {
         getName(comment['user']).then(nome => setname(nome))
     }
 
     return (
-        <View style={{ flexDirection: 'row', alignItems:'center', marginHorizontal:30 }}>
-            {(image == profileIcon) ?
-                (<Image source={profileIcon} style={{ height: 50, width: 43 }} />) :
-                (
-                    <Svg style={{
-                        width: 45,
-                        height: 45,
-                        borderRadius: 50,
-                        marginRight: 8
-                    }} width="50" height="50" viewBox="0 -3 43 55">
-                        <Polygon stroke='#F25C05' strokeWidth={5} points="0 10, 22.5 0, 45 10, 45 40, 22.5 50, 0 40" />
-                        <Defs>
-                            <ClipPath id="image" clipRule="evenodd">
-                                <Polygon points="0 10, 22.5 0, 45 10, 45 40, 22.5 50, 0 40" />
-                            </ClipPath>
-                        </Defs>
-                        <SvgImage
-                            x="0"
-                            y="0"
-                            width="50"
-                            height="50"
-                            href={image}
-                            clipPath="#image"
-                        />
-                    </Svg>
-                )}
-            <View>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={{ fontWeight: 'bold' }}>
-                        {name}:
-                </Text>
-                    <Text >
-                        {" " + comment['comment']}
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal:15, justifyContent:'space-between' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 15, marginTop: 20, width:Dimensions.get('window').width -130 }}>
+                {(image == profileIcon) ?
+                    (<Image source={profileIcon} style={{ height: 50, width: 43 }} />) :
+                    (
+                        <Svg style={{
+                            width: 45,
+                            height: 45,
+                            borderRadius: 50,
+                            marginRight: 8
+                        }} width="50" height="50" viewBox="0 -3 43 55">
+                            <Polygon stroke='#F25C05' strokeWidth={5} points="0 10, 22.5 0, 45 10, 45 40, 22.5 50, 0 40" />
+                            <Defs>
+                                <ClipPath id="image" clipRule="evenodd">
+                                    <Polygon points="0 10, 22.5 0, 45 10, 45 40, 22.5 50, 0 40" />
+                                </ClipPath>
+                            </Defs>
+                            <SvgImage
+                                x="0"
+                                y="0"
+                                width="50"
+                                height="50"
+                                href={image}
+                                clipPath="#image"
+                            />
+                        </Svg>
+                    )}
+                <View style={{}}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={{ fontWeight: 'bold' }}>
+                            {name}:
+                        </Text>
+                        <Text >
+                            {" " + comment['comment']}
+                        </Text>
+                    </View>
+                    <Text style={{ color: 'gray' }}>
+                        {getTime()}
                     </Text>
                 </View>
-                <Text style={{color:'gray'}}>
-                    {getTime()}
-                </Text>
             </View>
+            {canDelete && <FontAwesome name="trash" size={24} color="red" onPress={() => {Alert.alert(
+                                            'Excluir comentário?',
+                                            'Você tem certeza que deseja excluir este comentário?',
+                                            [
+                                                {
+                                                    text: 'Excluir',
+                                                    onPress: () => deleteComment()
+                                                },
+                                                {
+                                                    text: 'Cancelar',
+                                                    onPress: () => console.log('Cancel Pressed'),
+                                                    style: 'cancel'
+                                                }
+                                            ],
+                                            { cancelable: true }
+                                        );}}/>}
         </View>
     )
 }
@@ -101,18 +126,19 @@ export default function Comments({ navigation, route }) {
     const [comments, setcomments] = useState(post['comments'])
     const user = firebase.auth().currentUser.uid
 
+    setTimeout(() => {
+        firebase.firestore().collection('posts').doc(post['postname']).get().then(data => {setcomments(data.data()['comments'])})
+    }, 1000);
     async function comentar() {
         var comentario = {
             user,
             comment,
-            timestamp: Date.now()
+            timestamp: Date.now(),
+            postname:post['postname']
         }
         if (comment.length > 0) {
-            var cm = comments
-            cm.push(comentario)
-            await firebase.firestore().collection('posts').doc(post['postname']).update({ comments: firebase.firestore.FieldValue.arrayUnion(comentario) })
             setcomment("")
-            setcomments(cm)
+            await firebase.firestore().collection('posts').doc(post['postname']).update({ comments: firebase.firestore.FieldValue.arrayUnion(comentario) })
         }
     }
     return (
@@ -121,8 +147,10 @@ export default function Comments({ navigation, route }) {
             showsVerticalScrollIndicator={false}>
             <Header />
             <ScrollView>
-                {comments.map(item => (<RenderComment comment={item} />))}
-                <View style={{ flexDirection: 'row', marginTop: 50, marginHorizontal: 30, alignItems: 'center', justifyContent: 'center' }}>
+                <ScrollView style={{ height: Dimensions.get('window').height - 200 }}>
+                    {comments.map(item => (<RenderComment comment={item} />))}
+                </ScrollView>
+                <View style={{ flexDirection: 'row', marginTop: 10, marginHorizontal: 30, alignItems: 'center', justifyContent: 'center' }}>
                     <MyTextInput
                         onChangeText={text => setcomment(text)}
                         value={comment}
