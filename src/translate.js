3863d2b0d70ba69bc5627b6e1f1af02ee1ba675c
viguime <jvictormguimaraes@gmail.com>
2020-11-17 11:27:23 -0300

    translate file

---
+++ src/translate.js
@@ -0,0 +1,56 @@
+const translate = {
+    en: {
+        passw: 'Password',
+        emailfield: 'Type your email',
+        passfield: 'Type your password',
+        recopass: 'I forgot my password',
+        fblogin: 'login with facebook',
+        glogin: 'login with google',
+        createacc: 'create an account',
+        passalert: 'User does not exist or password is incorrect!',
+        erroralert: 'An error occurred',
+        cancelalert: 'The user canceled',
+
+        welcome:'Welcome',
+        phrase:'Let\'s begin creating your account',
+        fullname:'Full Name',
+        
+        rpassw:'Repeat the password',
+        next:'NEXT',
+        verpass:'"Please check the passwords provided!", "The passwords provided are not the same!\n\nTry again!"',
+      },
+      pt: {
+        passw: 'Senha',
+        emailfield: 'Informe seu email',
+        passfield: 'Informe sua senha',
+        recopass: 'Esqueci minha senha',
+        fblogin: 'entrar com o facebook',
+        glogin: 'entrar com o google',
+        createacc: 'criar uma conta',
+        passalert: 'O usuário não existe ou a senha está incorreta!',
+        erroralert: 'Ocorreu um erro',
+        cancelalert: 'O usuário cancelou',
+
+        welcome:'Seja Bem Vindo(a)',
+        phrase:'Vamos iniciar a criação da sua conta',
+        fullname:'Nome Completo',
+        passw:'Senha',
+        rpassw:'Repita a senha',
+        next: 'PRÓXIMO',
+        verpass:'"Verifique as senhas inseridas!", "As senhas inseridas não são iguais!\n\nTente outra vez!"',
+      },
+      es: {
+        passw: 'Contraseña',
+        emailfield: 'Ingrese su correo electrónico',
+        passfield: 'Ingrese su contraseña',
+        recopass: 'Olvidé mi contraseña',
+        fblogin: 'iniciar sesión con facebook',
+        glogin: 'iniciar sesión con google',
+        createacc: 'crear una cuenta',
+        passalert: '¡El usuario no existe o la contraseña es incorrecta!',
+        erroralert: 'Se ha producido un error',
+        cancelalert: 'El usuario ha cancelado',
+        },
+}
+
+export default translate
