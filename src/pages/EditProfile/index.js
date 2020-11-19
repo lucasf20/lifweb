3daea5d4d2ac9d37c2014149d006f599c33d71d3
viguime <jvictormguimaraes@gmail.com>
2020-11-19 10:44:24 -0300

    bugfix editprofile

--- src/pages/EditProfile/index.js
+++ src/pages/EditProfile/index.js
@@ -577,7 +577,7 @@ function Part3({ changeState }) {
                 return reg.test(item.toUpperCase())
             })
 
-            list.push(i18n.t('notonthelist'))
+            lista.push(i18n.t('notonthelist'))
 
             for (let i = 0; i < keys.length; i++) {
                 lista.push(profissoes[keys[i]]['titulo'])
